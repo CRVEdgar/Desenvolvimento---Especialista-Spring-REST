e30ac83ad68f9104524126f0cef5780fc7763097
edgar <edgarmelo@acad.ifma.edu.br>
2021-08-09 12:08:01 -0300

    personalizando a interface da Cozinha para realizar query com keywords

--- src/main/java/com/algafood/algafoodapiaplication/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algafood/algafoodapiaplication/domain/repository/CozinhaRepository.java
@@ -5,14 +5,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
+    //o nome do metodo tem que ser igual a um nome de variavel da classe | ou usar o findBy como prefixo
+    //keywords [containing = like % ]
+    List<Cozinha> findTodasByNomeContaining(String nome);
 
+    Optional<Cozinha> findByNome(String nome);
 
 //    List<Cozinha> listar();
-//    List<Cozinha> consultarPorNome(String nome);
+
 //    Cozinha buscar (Long id);
 //    Cozinha adicionarOuAtualizar(Cozinha cozinha);
 //    void remover(Long id);
