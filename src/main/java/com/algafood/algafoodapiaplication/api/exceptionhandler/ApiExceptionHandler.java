14aaabc5efa720ef6fb27a293c102167212e15b9
edgar <edgarmelo@acad.ifma.edu.br>
2021-08-23 09:25:04 -0300

    tratando exceções de forma global part2

--- src/main/java/com/algafood/algafoodapiaplication/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algafood/algafoodapiaplication/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,2 +1,51 @@
-package com.algafood.algafoodapiaplication.api.exceptionhandler;public class ApiExceptionHandler {
+package com.algafood.algafoodapiaplication.api.exceptionhandler;
+
+import com.algafood.algafoodapiaplication.domain.exception.EntidadeNaoEncontradaException;
+import com.algafood.algafoodapiaplication.domain.exception.NegocioException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+/*
+* PONTO CENTRAL PARA TRATAMENTO DE EXCEPTONHANDLER
+* */
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(EntidadeNaoEncontradaException.class)
+    public ResponseEntity<?> tratarEntidadeNaoEncontradaException(
+            EntidadeNaoEncontradaException e){
+
+        Problema problema = Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
+    }
+
+    @ExceptionHandler(NegocioException.class)
+    public ResponseEntity<?> tratarNegocioException(
+            NegocioException e){
+
+        Problema problema = Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem(e.getMessage())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<?> tratarHttpMEdiaTypeNotSupportedException(){
+        Problema problema = Problema.builder()
+                .dataHora(LocalDateTime.now())
+                .mensagem("Tipo de midia não suportado! ").build();
+
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(problema);
+    }
 }
