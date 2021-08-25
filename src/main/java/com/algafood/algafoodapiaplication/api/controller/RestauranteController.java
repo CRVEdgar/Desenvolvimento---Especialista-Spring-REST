5dbb905ddde1f6c1e3ac1ec51a54cea38fe168d5
Edgar Melo <edgarmelo@acad.ifma.edu.br>
2021-08-24 23:24:41 -0300

    Lançando exception de desserialização na atualização parcial (PATCH)

--- src/main/java/com/algafood/algafoodapiaplication/api/controller/RestauranteController.java
+++ src/main/java/com/algafood/algafoodapiaplication/api/controller/RestauranteController.java
@@ -8,14 +8,19 @@ import com.algafood.algafoodapiaplication.domain.model.Cozinha;
 import com.algafood.algafoodapiaplication.domain.model.Restaurante;
 import com.algafood.algafoodapiaplication.domain.repository.RestauranteRepository;
 import com.algafood.algafoodapiaplication.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
@@ -86,31 +91,42 @@ public class RestauranteController {
     }
 
     @PatchMapping("/{restauranteId}")
-    public Restaurante atualizaParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos){
+    public Restaurante atualizaParcial(@PathVariable Long restauranteId, @RequestBody Map<String, Object> campos, HttpServletRequest request){
         Restaurante restauranteAtual = cadastroRestaurante.buscarOuFalhar(restauranteId);
 
-        merge(campos, restauranteAtual);
+        merge(campos, restauranteAtual, request);
 
         return atualizar(restauranteId, restauranteAtual);
     }
 
-    //explicação VD 4.34
-    private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino){
-        ObjectMapper objectMapper = new ObjectMapper();
-        Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+    //explicação VD 4.34 /8.24
+    private void merge(Map<String, Object> dadosOrigem, Restaurante restauranteDestino, HttpServletRequest request){
+        ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
 
-        System.out.println(restauranteOrigem);
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
 
-        dadosOrigem.forEach((nomePropriedade, valorPropriedade) ->{
-            Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-            field.setAccessible(true); //permite acesso à variavel nome (que é privada)
+            objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
 
-            Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+            Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
 
-            System.out.println(nomePropriedade + " = " + valorPropriedade + " novo valor:" + novoValor);
+            System.out.println(restauranteOrigem);
 
-           ReflectionUtils.setField(field, restauranteDestino, novoValor);
-        });
+            dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+                Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+                field.setAccessible(true); //permite acesso à variavel nome (que é privada)
+
+                Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+                System.out.println(nomePropriedade + " = " + valorPropriedade + " novo valor:" + novoValor);
+
+                ReflectionUtils.setField(field, restauranteDestino, novoValor);
+            });
+        }catch(IllegalArgumentException e){
+            Throwable rootCause = ExceptionUtils.getRootCause(e);
+            throw new HttpMessageNotReadableException(e.getMessage(),rootCause, serverHttpRequest);
+        }
     }
 
 }
