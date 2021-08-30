1b948b8994d455673d9c0baaf98f743b4e33cf31
edgar <edgarmelo@acad.ifma.edu.br>
2021-08-30 12:10:28 -0300

    adicionando validação das classes de Cidade e Estado

--- src/main/java/com/algafood/algafoodapiaplication/api/controller/CidadeController.java
+++ src/main/java/com/algafood/algafoodapiaplication/api/controller/CidadeController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 
@@ -40,7 +41,7 @@ public class CidadeController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cidade adicionar (@RequestBody Cidade cidade){
+    public Cidade adicionar (@RequestBody @Valid Cidade cidade){
         try{
             return cadastroCidade.salvar(cidade);
         }catch (EstadoNaoEncontradoException e){
@@ -51,7 +52,7 @@ public class CidadeController {
 
 
     @PutMapping("/{cidadeId}")
-    public Cidade atualizar( @PathVariable Long cidadeId, @RequestBody Cidade cidade){
+    public Cidade atualizar( @PathVariable Long cidadeId, @RequestBody @Valid Cidade cidade){
         try{
             Cidade cidadeAtual = cadastroCidade.buscarOuFalhar(cidadeId);
 
--- src/main/java/com/algafood/algafoodapiaplication/domain/model/Estado.java
+++ src/main/java/com/algafood/algafoodapiaplication/domain/model/Estado.java
@@ -1,10 +1,13 @@
 package com.algafood.algafoodapiaplication.domain.model;
 
+import com.algafood.algafoodapiaplication.Groups;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -12,11 +15,13 @@ import javax.persistence.*;
 @Table(name ="tab_estado")
 public class Estado {
 
+    @NotNull(groups = Groups.EstadoId.class)
     @EqualsAndHashCode.Include
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     @Column(nullable=false)
     private String nome;
 }
