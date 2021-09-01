f8e101546bc76603e735399c4069c26c1b62173a
edgar <edgarmelo@acad.ifma.edu.br>
2021-09-01 12:06:51 -0300

    adicionando flyway - part1 | TODO

--- src/test/java/com/algafood/algafoodapiaplication/CadastroCozinhaIT.java
+++ src/test/java/com/algafood/algafoodapiaplication/CadastroCozinhaIT.java
@@ -12,11 +12,14 @@ import com.algafood.algafoodapiaplication.domain.service.CadastroCozinhaService;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 //import org.hamcrest.Matchers;
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 
 import javax.validation.ConstraintViolationException;
 
@@ -26,16 +29,25 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CadastroCozinhaIT {
 
+    //private static Flyway flyway;
     @Autowired
     private CadastroCozinhaService cadastroCozinha;
 
+    @Autowired
+    private static Flyway flyway;
+
     @BeforeAll
-    public static void setUp(){
+    //@Test
+    private static void setUp(){
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails(); //auxilia no Debug mostrando o que foi realizado no metodo //habilita os logs quando o teste falhar
         RestAssured.port = port; //especifica a porta padrao
         RestAssured.basePath = "/cozinhas";
+
+        //flyway.migrate(); //utilizado para restartar o BD a cada teste que for realizado
     }
 
+
+    //----------------------------------------------------
     // TESTES DE INTEGRAÇÃO
 
     @Test
@@ -117,6 +129,18 @@ public class CadastroCozinhaIT {
             .get()
         .then()
             .body("titulo", hasItems("Brasileira", "Australiana", "Tailandesa")); //valida se há as cozinhas especificadas
+    }
+
+    @Test
+    public void testRetornarStatus201_QuandoCadastrarCozinha() {
+        given()
+            .body("{ \"nome\": \"Chinesa\" }")
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+        .when()
+            .post()
+        .then()
+            .statusCode(HttpStatus.CREATED.value());
     }
 
 }
